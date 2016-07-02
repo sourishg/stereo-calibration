00ac8a98bae7d9b2adbc5fffb25153e4ed32054e
sourishg <ragnarok0211@gmail.com>
2016-07-02 19:16:31 -0400

    Add popt to undistort_rectify

--- undistort_rectify.cpp
+++ undistort_rectify.cpp
@@ -4,20 +4,41 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
 #include <iostream>
+#include "popt_pp.h"
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char const *argv[])
 {
-  cv::Mat R1, R2, P1, P2, Q;
-  cv::Mat K1, K2, R;
-  cv::Vec3d T;
-  cv::Mat D1, D2;
-  cv::Mat img1 = imread("../left10.png", CV_LOAD_IMAGE_COLOR);
-  cv::Mat img2 = imread("../right10.png", CV_LOAD_IMAGE_COLOR);
+  char* leftimg_filename;
+  char* rightimg_filename;
+  char* calib_file;
+  char* leftout_filename;
+  char* rightout_filename;
 
-  cv::FileStorage fs1("cam_stereo.yml", cv::FileStorage::READ);
+  static struct poptOption options[] = {
+    { "leftimg_filename",'l',POPT_ARG_STRING,&leftimg_filename,0,"Left imgage path","STR" },
+    { "rightimg_filename",'r',POPT_ARG_STRING,&rightimg_filename,0,"Right image path","STR" },
+    { "calib_file",'c',POPT_ARG_STRING,&calib_file,0,"Stereo calibration file","STR" },
+    { "leftout_filename",'L',POPT_ARG_STRING,&leftout_filename,0,"Left undistorted imgage path","STR" },
+    { "rightout_filename",'R',POPT_ARG_STRING,&rightout_filename,0,"Right undistorted image path","STR" },
+    POPT_AUTOHELP
+    { NULL, 0, 0, NULL, 0, NULL, NULL }
+  };
+
+  POpt popt(NULL, argc, argv, options, 0);
+  int c;
+  while((c = popt.getNextOpt()) >= 0) {}
+
+  Mat R1, R2, P1, P2, Q;
+  Mat K1, K2, R;
+  Vec3d T;
+  Mat D1, D2;
+  Mat img1 = imread(leftimg_filename, CV_LOAD_IMAGE_COLOR);
+  Mat img2 = imread(rightimg_filename, CV_LOAD_IMAGE_COLOR);
+
+  cv::FileStorage fs1(calib_file, cv::FileStorage::READ);
   fs1["K1"] >> K1;
   fs1["K2"] >> K2;
   fs1["D1"] >> D1;
@@ -38,21 +59,9 @@ int main(int argc, char const *argv[])
   cv::initUndistortRectifyMap(K2, D2, R2, P2, img2.size(), CV_32F, rmapx, rmapy);
   cv::remap(img1, imgU1, lmapx, lmapy, cv::INTER_LINEAR);
   cv::remap(img2, imgU2, rmapx, rmapy, cv::INTER_LINEAR);
+  
+  imwrite(leftout_filename, imgU1);
+  imwrite(rightout_filename, imgU2);
 
-  imshow("image1", imgU1);
-  imshow("image2", imgU2);
-  
-  Mat imgU1G, imgU2G;
-  cvtColor(imgU1, imgU1G, CV_BGR2GRAY);
-  cvtColor(imgU2, imgU2G, CV_BGR2GRAY);
-  
-  imwrite("../left_rect10.pgm", imgU1G);
-  imwrite("../right_rect10.pgm", imgU2G);
-  
-  int k = waitKey(0);
-  if (k == ' ') {
-    imwrite("../left_rect10.png", imgU1);
-    imwrite("../right_rect10.png", imgU2);
-  }
   return 0;
 }
